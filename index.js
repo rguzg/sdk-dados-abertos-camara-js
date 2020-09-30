cada1a96f5bebfb99ac9fc84ab8f60d260c0e22a
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-09-30 22:21:38 +0200

    :bulb: Add eventos endpoints wrappers
    Fix #4

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import blocos from './modules/blocos';
 import deputados from './modules/deputados';
+import eventos from './modules/eventos';
 
 export { blocos };
 
 const CamaraAPI = {
   ...blocos,
   ...deputados,
+  ...eventos,
 };
 
 export default CamaraAPI;
